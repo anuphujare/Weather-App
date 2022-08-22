43f7d15a29e767e5ca423bf3d8fb299cd2e9e345
prosolver9604 <102342364+prosolver9604@users.noreply.github.com>
2022-08-22 23:49:30 +0530

    updated units of pressure and humidity

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -35,11 +35,11 @@ const Forecast = ({ data }) => {
             <div className="daily-details-grid">
                 <div className="daily-details-grid-item">
                   <label>Pressure:</label>
-                  <label>{item.main.pressure}</label>
+                  <label>{item.main.pressure} hPa</label>
                 </div>
                 <div className="daily-details-grid-item">
                   <label>Humidity:</label>
-                  <label>{item.main.humidity}</label>
+                  <label>{item.main.humidity}%</label>
                 </div>
                 <div className="daily-details-grid-item">
                   <label>Clouds:</label>
